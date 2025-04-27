59a2e212c3baa2c4243ab115eec5f87904d593a8
Gabriel Peloso <pelosojoaogabriel@gmail.com>
2025-04-27 16:12:45 -0300

    Adicionar arquivos de projeto.

---
+++ TaskPomodoro.API/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskPomodoro.API.Models;
+
+namespace TaskPomodoro.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ TaskPomodoro.API/Models/Tarefa.cs
@@ -0,0 +1,24 @@
+﻿namespace TaskPomodoro.API.Models
+{
+    public class Tarefa
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataConclusao { get; set; }
+        public bool Concluida { get; set; }
+        public int TempoEstimado { get; set; } // em minutos
+        public int TempoGasto { get; set; } // em minutos
+        public int PomodorosConcluidos { get; set; }
+        public int PomodorosRestantes { get; set; }
+        public Tarefa()
+        {
+            DataCriacao = DateTime.Now;
+            Concluida = false;
+            PomodorosConcluidos = 0;
+            PomodorosRestantes = 0;
+        }
+    }
+}
